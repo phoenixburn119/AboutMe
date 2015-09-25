115eb3a6371edacbcd557ce19a34aa78b59f66fc
Adam Kinzer <adamkinzer915@gmail.com>
2015-09-25 13:51:28 -0600

    Created AboutMe

---
+++ app/src/main/java/ctec/aboutme/TitleScreenActivity.java
@@ -0,0 +1,54 @@
+package ctec.aboutme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_title_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupListeners()
+    {
+        startButton.setOnCLickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View buttonView)
+            {
+
+            }
+        }
+    }
+}
